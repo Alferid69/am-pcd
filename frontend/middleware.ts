aa349f3bf1828b36befe9de7f558fabb576ad926
Alferid Hassen <nsr-1452-14@amu.edu.et>
2026-05-09 16:22:15 +0300

    fix: Correct variable name for i18n configuration in middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,7 @@
 import { createProxy } from 'next-i18next/proxy';
-import config from './i18n.config';
+import i18nConfig from './i18n.config';
 
-export default createProxy(config);
+export default createProxy(i18nConfig);
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
