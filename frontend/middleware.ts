885e2eec794bda33fb929f7c53da5861a9c19c6b
Alferid Hassen <nsr-1452-14@amu.edu.et>
2026-07-11 11:50:08 +0300

    fix: Update middleware matcher to exclude sitemap.xml and robots.txt

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,5 +4,5 @@ import i18nConfig from './i18n.config';
 export default createProxy(i18nConfig);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
